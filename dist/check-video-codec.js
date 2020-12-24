89fba3db4e9ea043e2dee3de01be7a0dd1734053
Vladimir Shapovalov <vladimir.shapovalov@nimbusweb.co>
2020-12-24 11:23:12 +0700

    change function for check codec

--- dist/check-video-codec.js
+++ dist/check-video-codec.js
@@ -2,50 +2,41 @@ const MP4Box = require('mp4box')
 const MediaInfo = require('mediainfo.js')
 
 /**
- * Класс CheckMp4BoxCodec
- * Предназначен для получения медиаинформации с помощью mp4box.js
+ * Class CheckMp4BoxCodec
+ * For check video codec by mp4box.js
  */
 class CheckMp4BoxCodec {
-    mp4BoxFile = null
-    mp4BoxInfo = null
-    mp4BoxFileStart = 0
-    chunksLength = 0;
-
-    constructor() {
-        this.initMp4Box()
-    }
-
-    /**
-     * Инициализация mp4box
-     */
-    initMp4Box = () => {
+    constructor () {
         this.mp4BoxFile = MP4Box.createFile()
-        this.mp4BoxFile.onError = this.onMp4BoxError
-        this.mp4BoxFile.onReady = this.onMp4BoxReady
+        this.mp4BoxFile.onError = this.onMp4BoxError.bind(this)
+        this.mp4BoxFile.onReady = this.onMp4BoxReady.bind(this)
+        this.mp4BoxInfo = null
+        this.mp4BoxFileStart = 0
+        this.chunksLength = 0;
     }
 
     /**
-     * Событие при ошибке mp4box
+     * Event after catch error in mp4box
      * @param err
      */
-    onMp4BoxError = (err) => {
+    onMp4BoxError (err) {
         console.error(err)
     }
 
     /**
-     * Событие после того как информация о кодеках прочитана
+     * Event after get codec information
      * @param info
      */
-    onMp4BoxReady = (info) => {
+    onMp4BoxReady (info) {
         this.mp4BoxInfo = info
     }
 
     /**
-     * Конвертирует Buffer в ArrayBuffer
+     * Convert Buffer to ArrayBuffer
      * @param {Buffer} buf
      * @returns {ArrayBuffer}
      */
-    toArrayBuffer = (buf) => {
+    toArrayBuffer (buf) {
         const ab = new ArrayBuffer(buf.length);
         const view = new Uint8Array(ab);
         for (let i = 0; i < buf.length; ++i) {
@@ -55,58 +46,70 @@ class CheckMp4BoxCodec {
     }
 
     /**
-     * Проверка с помощью mp4box.js
+     * Check by mp4box.js
      * @param {Buffer} buf
      * @returns {Object}
      */
-    check = async (buf) => {
+    async check (buf) {
         const ab = this.toArrayBuffer(buf)
         ab.fileStart = this.mp4BoxFileStart
         this.mp4BoxFile.appendBuffer(ab)
         this.mp4BoxFileStart += buf.length
         this.chunksLength ++
 
-        return new Promise((resolve, reject) => {
+        return new Promise((function (resolve, reject) {
             resolve(this.mp4BoxInfo)
-        })
+        }).bind(this))
+    }
+
+    clear () {
+        this.mp4BoxFile.flush()
     }
 }
 
 /**
- * Класс CheckMediainfoCodec
- * Предназначен для получения медиаинформации с помощью mediainfo.js
+ * Class CheckMediainfoCodec
+ * For check codec by mediainfo.js
  */
 class CheckMediainfoCodec {
-    chunks = []
-    mediainfo = null
-
-    /**
-     * Инициализация mediainfo
-     * @returns {Promise<any>}
-     */
-    initMediaInfo = async () => {
-        return new Promise((resolve, reject) => {
-            MediaInfo({ format: 'JSON' }, (mediainfo) => {
-                this.mediainfo = mediainfo
-                resolve(mediainfo)
-            })
-        })
+    constructor() {
+        this.chunks = []
+        this.mediainfo = null
     }
 
     /**
-     * Проверка с помощью mediainfo.js
+     * Init of mediainfo
+     * @returns {Promise<any>}
+     */
+     async initMediaInfo () {
+        return new Promise((function (resolve, reject) {
+            if(this.mediainfo) {
+                resolve(this.mediainfo)
+            } else { 
+                MediaInfo({ format: 'JSON' }, (function (mediainfo) {
+                    this.mediainfo = mediainfo
+                    resolve(mediainfo)
+                }).bind(this))
+            }
+        }).bind(this))
+    }
+
+    /**
+     * Checking with mediainfo.js
      * @param {Buffer} buf
      * @returns {Promise<boolean|*>}
      */
-    check = async (buf) => {
+    async check (buf) {
         if(!this.mediainfo) throw new Error('have to init mediainfo')
         this.chunks.push(buf)
 
         if (!this.mediainfo) return false
 
         const buffer = Buffer.concat(this.chunks)
+        const getSize = function() { return buffer.length }
+        const getBuffer = function() { return buffer }
 
-        const info = await this.mediainfo.analyzeData(() => buffer.length, () => buffer)
+        const info = await this.mediainfo.analyzeData(getSize, getBuffer)
         if (!info) return false;
 
         const json = JSON.parse(info)
@@ -116,79 +119,94 @@ class CheckMediainfoCodec {
 
         return false
     }
+
+    clear () {
+        this.chunks = []
+    }
 }
 
 /**
- * Класс CheckMp4Codec
- * Предназначен для получения информации от mp4box.js и mediainfo.js
+ * Class CheckMp4Codec
+ * For get information from mp4box.js и mediainfo.js
  */
 class CheckVideoCodec {
-    mp4box = null
-    mediainfo = null
-
-    constructor() {
+    
+    constructor(maxBufferSize=64*1024) {
+        this.len = 0
+        this.bufferSize = 0
+        this.maxBufferSize = maxBufferSize
         this.mp4box = new CheckMp4BoxCodec()
         this.mediainfo = new CheckMediainfoCodec()
     }
 
+    clear () {
+        this.mp4box.clear()
+        this.mediainfo.clear()
+        this.len = 0
+        this.bufferSize = 0
+    }
+
     /**
-     * Инициализируем mediainfo
+     * Init mediainfo
      * @returns {Promise<any>}
      */
-    init = async () => {
+    async init () {
         return this.mediainfo.initMediaInfo()
     }
 
     /**
-     * Функция проверки кодеков
+     * Check codecs
      * @param buf
      * @returns {Promise<any>}
      */
-    check = async (buf) => {
-        const res = await Promise.all([this.mp4box.check(buf), this.mediainfo.check(buf)])
-        if(res) {
-            const [mp4box, mi] = res
-            let codecs = {mime: null, general: null, video: null}
-            // Дожидаемся ответа от mp4box
-            if(!mp4box && mi && this.mp4box.chunksLength < 2) {
-                return null
-            }
+    async check (buf) {
+        this.len++
+        this.bufferSize = this.len*buf.length
+        
+        const [mp4box, mi] = await Promise.all([this.mp4box.check(buf), this.mediainfo.check(buf)])
+        let codecs = {mime: null, general: null, video: null}
 
-            if(mp4box) {
-                codecs = {mime: mp4box.mime}
-            }
-
-            if(mi) {
-                const [
-                    general,
-                    video
-                ] = mi.media.track
-
-                if(general) {
-                    codecs = {
-                        ...codecs, general: {
-                            format: general.Format,
-                            codec_id: general.CodecID,
-                            codec_id_compatible: general.CodecID_Compatible
-                        }
-                    }
-                }
-
-                if(video) {
-                    codecs = {
-                        ...codecs, video: {
-                            format: video.Format,
-                            codec_id: video.CodecID
-                        }
-                    }
-                }
-
-            }
-
-            return new Promise(resolve => resolve(codecs));
+        if (mp4box) {
+            codecs = {mime: mp4box.mime}
         }
 
-        return res
+        if (mi) {
+            const [
+                general,
+                video
+            ] = mi.media.track
+
+            if(general) {
+                codecs = {
+                    ...codecs, general: {
+                        format: general.Format,
+                        codec_id: general.CodecID,
+                        codec_id_compatible: general.CodecID_Compatible
+                    }
+                }
+            }
+
+            if(video) {
+                codecs = {
+                    ...codecs, video: {
+                        format: video.Format,
+                        codec_id: video.CodecID
+                    }
+                }
+            }
+
+            // trying to check by mp4box.js till bufferSize < maxBufferSize
+            if (!mp4box && this.bufferSize < this.maxBufferSize) {
+                // for .mov and webm skip checking of mp4box.js
+                if (general && general.Format !== 'WebM' && general.CodecID.trim() !== 'qt') {
+                    return false
+                }                
+            }
+
+            return codecs
+        }
+
+        return false
     }
 }
 
--- examples/index.js
+++ examples/index.js
@@ -1,20 +1,25 @@
 const fs = require('fs')
-const CheckMp4Codec = require('check-video-codec')
+const CheckMp4Codec = require('../dist/check-video-codec')
 
-const filePath = './video/android_open_vpn.mp4' // path to video
-const fileStream = fs.createReadStream(filePath)
+const filePath = './video/5.mov' // path to video
+const bufferSize = 1 // Kb
+const fileStream = fs.createReadStream(filePath, {highWaterMark: bufferSize*1024})
 
-const checkMp4Codec = new CheckMp4Codec()
+const checkMp4Codec = new CheckMp4Codec(64*1024)
+let len = 1;
 // waiting when mediainfo.js will be init
 checkMp4Codec.init().then( () => {
     let buf = Buffer.alloc(0)
     fileStream.on('data', async (chunk) => {
         buf = Buffer.concat([chunk])
+        
         // get information about video codec
         const res = await checkMp4Codec.check(buf)
         if (res) {
-            console.log(res)
-            fileStream.destroy()
+            console.log(JSON.stringify(res, null, 2))
+            fileStream.destroy()    
+            console.log('read Kb:', len*bufferSize)
         }
+        len++
     })
 })
